f4d61586bdcbdb317d9485e895778b24d65db1d8
Luke Ledvina <soulgp1@gmail.com>
2018-06-13 15:46:56 -0400

    Created Lifecycle methods

--- app/src/main/java/com/example/lukeledvina/lifecyclemethodexamples/MainActivity.java
+++ app/src/main/java/com/example/lukeledvina/lifecyclemethodexamples/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.lukeledvina.lifecyclemethodexamples;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button newActivityButton;
+    private Button finishProgramButton;
+
+    private Intent newActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
+        newActivityButton = findViewById(R.id.new_activity_button);
+        finishProgramButton = findViewById(R.id.finish_activity_button);
+
+        newActivity = new Intent(this, SecondActivity.class);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        newActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(newActivity);
+            } 
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
     }
 }
